34775d795ec23594f0ee48a11927c0eccd7334c7
irobles1030 <irobles1030@gmail.com>
2024-09-21 01:38:48 +0000

    image caching

--- api/controllers/sharpControllers.js
+++ api/controllers/sharpControllers.js
@@ -2,11 +2,13 @@ import * as sharpServices from '../services/sharpServices';
 
 export default {
     sharpImage: async (req, res) => {
+        const maxAge = 60 * 60 * 24 * 7;
         const { size, fileName } = req.params;
 
         const resizedImage = await sharpServices.sharpImage(size, fileName);
         const fileType = fileName.split('.')[1];
-
+        
+        res.setHeader('Cache-Control', `public, max-age=${maxAge}`);
         res.contentType(`image/${fileType}`);
         res.send(resizedImage);
     }

e1be9839c13b8df0ee781ccb8a2dcaa7c732b82a
roblesisaac <irobles1030@gmail.com>
2025-01-27 20:42:30 -0800

    Remove debug console logs from Etsy authentication and order fetching

--- api/controllers/etsyAuthController.js
+++ api/controllers/etsyAuthController.js
@@ -51,8 +51,6 @@ export const handleCallback = async (req, res) => {
     const tokenData = await etsyAuthService.getAccessToken(code, codeVerifier);
     const userInfo = await etsyAuthService.getUserInfo(tokenData.accessToken);
 
-    console.log('userInfo::', userInfo);
-
     // Store token data and user info in session
     req.session.etsyToken = {
       accessToken: tokenData.accessToken,

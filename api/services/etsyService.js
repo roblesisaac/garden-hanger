067c086813f26577422ec332b5131e877bf61439
roblesisaac <irobles1030@gmail.com>
2025-01-27 21:41:32 -0800

    Update Etsy order logging to print number of results

--- api/services/etsyService.js
+++ api/services/etsyService.js
@@ -82,7 +82,7 @@ export class EtsyService {
       }
 
       const data = await response.json();
-      console.log(JSON.stringify(data.results[0], null, 2));
+      console.log(JSON.stringify(data.results.length, null, 2));
       return data;
     } catch (error) {
       throw new Error(`Failed to fetch Etsy orders: ${error.message}`);

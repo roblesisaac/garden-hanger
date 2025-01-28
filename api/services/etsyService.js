6de360b5d37005e7b82288c97cdafc837bc327d1
roblesisaac <irobles1030@gmail.com>
2025-01-27 21:42:16 -0800

    Add console log to print data object keys in Etsy service

--- api/services/etsyService.js
+++ api/services/etsyService.js
@@ -82,6 +82,7 @@ export class EtsyService {
       }
 
       const data = await response.json();
+      console.log('data::', Object.keys(data));
       console.log(JSON.stringify(data.results.length, null, 2));
       return data;
     } catch (error) {

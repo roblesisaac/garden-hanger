dc26b04b9b4a7e54a85bb68e046df90cfaa4f0c0
roblesisaac <irobles1030@gmail.com>
2025-01-28 13:54:33 -0800

    Modify Etsy order sync to log existing order IDs

--- api/services/orderServices.js
+++ api/services/orderServices.js
@@ -239,9 +239,8 @@ export async function syncEtsyOrders(req) {
 
     // Get all existing Etsy orders first
     const existingOrders = await Orders.findAll({ orderSource: 'etsy' });
-    console.log('existingOrders', existingOrders.length);
     const existingOrderIds = new Set(existingOrders.map(order => order._id));
-
+    console.log('existingOrderIds', existingOrderIds);
     for (const etsyOrder of etsyOrders.results) {
       try {
         const etsyOrderId = `orders:${etsyOrder._id}`;

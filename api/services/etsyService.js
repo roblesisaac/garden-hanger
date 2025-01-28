49d715ae831d4d9e701960fd982118a580dc3081
roblesisaac <irobles1030@gmail.com>
2025-01-28 12:56:11 -0800

    debugs

--- api/services/etsyService.js
+++ api/services/etsyService.js
@@ -82,7 +82,6 @@ export class EtsyService {
       const transformedOrders = orders.results.map(order => {
         // Create ISO date string from Etsy timestamp
         console.log('order.created_timestamp', order.created_timestamp);
-        console.log('order.createdTimestamp', order.createdTimestamp);
         const orderDate = new Date(order.created_timestamp * 1000).toISOString();
         const dateForId = orderDate.replace(/[:]/g, '-');
         console.log('orderDate', orderDate);

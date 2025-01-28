08c24c7ce543a7e13bfb5060d9519dc19fd19ebd
roblesisaac <irobles1030@gmail.com>
2025-01-27 21:40:39 -0800

    Update Etsy order logging to print first result with formatted JSON

--- api/services/etsyService.js
+++ api/services/etsyService.js
@@ -82,7 +82,7 @@ export class EtsyService {
       }
 
       const data = await response.json();
-      console.log(Object.keys(data));
+      console.log(JSON.stringify(data.results[0], null, 2));
       return data;
     } catch (error) {
       throw new Error(`Failed to fetch Etsy orders: ${error.message}`);

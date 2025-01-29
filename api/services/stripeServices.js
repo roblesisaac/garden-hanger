ba4d76b76c6e77ba12ce163ad8cdd52d2f80ce83
roblesisaac <irobles1030@gmail.com>
2025-01-28 16:56:30 -0800

    debug

--- api/services/stripeServices.js
+++ api/services/stripeServices.js
@@ -83,6 +83,7 @@ export async function retreiveStripeSession(stripeSessionId) {
 
 export function retreiveStripePublicKey() {
   // return config.STRIPE.PUBLIC_TEST;
+  console.log('isProd()', isProd(), config.STRIPE.PUBLIC_TEST);
   return isProd() ? config.STRIPE.PUBLIC : config.STRIPE.PUBLIC_TEST;
 }
 
